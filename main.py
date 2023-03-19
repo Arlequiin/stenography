68697f9961ba0274f95a2bca9aca0de9817b636b
αρλ <78297845+Arlequiin@users.noreply.github.com>
2023-03-19 12:52:39 +0000

    backup before switch to post

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ def test():
 
 @app.route("/encodage/<dropdown_value>/<textarea_value>")
 def my_route(dropdown_value,textarea_value):
-    d = {'peche':'1.png','riviere':'2.png','hiver':'3.png'}
+    d = {'peche':'1.png','riviere':'2.png','hiver':'3.png','pro1':'4.png'}
     encode_text(textarea_value,'static/'+d[dropdown_value])
     return "Success"
 
 @app.route("/decodage/<dropdown_value>")
 def my_route2(dropdown_value):
-    d = {'peche':'1.png','riviere':'2.png','hiver':'3.png'}
+    d = {'peche':'1.png','riviere':'2.png','hiver':'3.png','pro1':'4.png'}
     print("Function detected")
     decoded=decode_text('static/'+d[dropdown_value])
     print(decoded)
